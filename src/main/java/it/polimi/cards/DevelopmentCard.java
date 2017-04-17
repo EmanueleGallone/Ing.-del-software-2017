764ee49a994d6ca22a2bfacd874bb9764db8e587
PierpaoloM01 <pierpaolo.mancini@mail.polimi.it>
2017-04-17 16:27:04 +0200

    Primo tentativo di implementazione degli eventi

--- src/main/java/it/polimi/cards/DevelopmentCard.java
+++ src/main/java/it/polimi/cards/DevelopmentCard.java
@@ -1,8 +1,11 @@
 package it.polimi.cards;
 
+import event.Activable;
+import event.Event;
+import event.Observer;
 import it.polimi.ingsw.resources.Resource;
 
-public class DevelopmentCard extends Card {
+public class DevelopmentCard extends Card implements Activable {
 	protected static final int MAX_DECK=96;
 	protected static final int DECK_MAX_CARDS_PER_PERIOD=32;
 	public static final int MAX_YELLOW_CARDS=6;
@@ -15,13 +18,30 @@ public class DevelopmentCard extends Card {
 	protected Resource requirements;
 	protected int period;
 	
+	protected Event activatedEvent = new Event(); //Oggetto che definisce l'evento "Attivato"
 	
 	@Override
 	public String toString() {
 		return "DevelopmentCard [name=" + name + ", colour=" + colour + ", resourceAffected=" + resourceAffected
 				+ ", description=" + description + ", requirements=" + requirements + ", period=" + period + "]";
 	}
-	
-	
 
+
+	@Override
+	public void active() {
+		activatedEvent.preEventNotify();
+		// Do something
+		activatedEvent.postEventNotify();
+	}
+	
+	@Override //Metodo che serve ad un "observer" per iniziare ad osservare l'evento "activated"
+	public void observActiveEvent(Observer observer) {
+		activatedEvent.attach(observer);
+	}
+
+	@Override //Metodo per smettere di osservare l'evento "activated"
+	public void deobservActiveEvent(Observer observer) {
+		activatedEvent.deatach(observer);
+	}
+	
 }
