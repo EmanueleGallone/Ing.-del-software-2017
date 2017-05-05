13c3d38c93851b4c7d333e5f81f2d859420d1749
gabss <gabriel.logronio@mail.polimi.it>
2017-05-05 17:59:06 +0200

    Test Server

---
+++ src/main/java/ProvaServerGame/Metodo_di_esecuzione.java
@@ -0,0 +1,16 @@
+package ProvaServerGame;
+
+public class Metodo_di_esecuzione {
+
+	/* Parte il server e si mette in ascolto di connessioni in arrivo sulla porta tramite run() in ciclo continuo.
+	 * Ogni volta che una si connette, si crea il client che richiede un nome e abilita lo scambio di messaggi con il server.
+	 * Inserisce il client tra le connessioni in attesa (waitingGame).
+	 * Quando ci sono almeno 2 connessioni in attesa, si crea la lobby. 
+	 * Se ce ne sono più di 2 le passa direttamente alla lobby.
+	 * La lobby aspetta tutte le connessioni in arrivo fino ad un massimo di altre 2 per 60 secondi.
+	 * Arrivati al max di giocatori o scaduto il timer, crea la partita e ripulisce la lista dei giocatori in attesa.
+	 * La partita crea board, view di ogni giocatore e setto il modello MVC
+	 */
+	
+	
+}
