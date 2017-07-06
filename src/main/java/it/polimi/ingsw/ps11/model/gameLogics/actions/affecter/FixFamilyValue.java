133068cf9bb54b70a528b2396eca3b5d99f1fad3
PierpaoloM01 <pierpaolo.mancini@mail.polimi.it>
2017-07-06 11:17:45 +0200

    small fix in fixedValuAffecter

--- src/main/java/it/polimi/ingsw/ps11/model/gameLogics/actions/affecter/FixFamilyValue.java
+++ src/main/java/it/polimi/ingsw/ps11/model/gameLogics/actions/affecter/FixFamilyValue.java
@@ -13,6 +13,7 @@ public class FixFamilyValue extends FamilyInSpaceAffecter {
 		FamilyInSpaceAction newAction = action.clone();
 		int currentValue = newAction.getFamilyMember().getValue();
 		int currentModifier = newAction.getFamilyMember().getModifier(); 	//Serve a tenere conto dell'effetto di altri bonus che hanno alterato il modificatore
+		currentValue = currentValue - currentModifier;						//Serve ad ottenere il valore effettivo dei dadi
 		int difference = this.value - currentValue;							
 		newAction.getFamilyMember().setModifier(difference + currentModifier );
 		return newAction;
