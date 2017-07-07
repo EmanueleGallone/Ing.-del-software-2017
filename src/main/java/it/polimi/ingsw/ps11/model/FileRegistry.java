206b3b5d78ced2552e129bb4c621c10f14060893
PierpaoloM01 <pierpaolo.mancini@mail.polimi.it>
2017-07-07 17:30:24 +0200

    fixed file path

--- src/main/java/it/polimi/ingsw/ps11/model/FileRegistry.java
+++ src/main/java/it/polimi/ingsw/ps11/model/FileRegistry.java
@@ -1,16 +1,18 @@
 package it.polimi.ingsw.ps11.model;
 
+import java.io.File;
+
 public class FileRegistry {
 
-	public static final String root = "settings\\";
+	public static final String root = "settings" + File.separatorChar;
 	public static final String board = FileRegistry.root + "board";
-	public static final String cards = FileRegistry.root + "cards\\";
-	public static final String timers_lobby = FileRegistry.root + "timers\\lobby";
-	public static final String timers_turn = FileRegistry.root + "timers\\turn";
-	public static final String view_commands = FileRegistry.root + "view\\commands";
-	public static final String login_registry = FileRegistry.root + "login\\register";
+	public static final String cards = FileRegistry.root + "cards" + File.separatorChar;
+	public static final String timers_lobby = FileRegistry.root + "timers"+ File.separatorChar + "lobby";
+	public static final String timers_turn = FileRegistry.root + "timers" + File.separatorChar + "turn";
+	public static final String view_commands = FileRegistry.root + "view" + File.separatorChar + "commands";
+	public static final String login_registry = FileRegistry.root +"login"+ File.separatorChar +"register";
 	public static final String player = FileRegistry.root + "player";
-	public static final String church = FileRegistry.root + "church\\" + "church";
+	public static final String church = FileRegistry.root + "church" + File.separatorChar + "church";
 	public static final String leaderCards = cards + "leaderCards";
-	public static final String excommunication = FileRegistry.root + "church\\" + "excommunication";
+	public static final String excommunication = FileRegistry.root + "church" + File.separatorChar + "excommunication";
 }
