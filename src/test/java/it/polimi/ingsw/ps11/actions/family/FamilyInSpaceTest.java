c7c6d98ae0a69c5a8d80c49c293d6d455c9073d8
EmanueleGallone <emanuele.gallone@mail.polimi.it>
2017-07-10 01:47:39 +0200

    added tests

--- src/test/java/it/polimi/ingsw/ps11/actions/family/FamilyInSpaceTest.java
+++ src/test/java/it/polimi/ingsw/ps11/actions/family/FamilyInSpaceTest.java
@@ -13,6 +13,7 @@ import it.polimi.ingsw.ps11.model.gameLogics.actions.ActionManager;
 import it.polimi.ingsw.ps11.model.gameLogics.actions.family.FamilyInSpaceAction;
 import it.polimi.ingsw.ps11.model.player.Player;
 import it.polimi.ingsw.ps11.model.zones.actionSpace.ActionSpace;
+import it.polimi.ingsw.ps11.view.viewEvents.ConfirmViewEvent;
 
 public class FamilyInSpaceTest {
 	
@@ -39,12 +40,24 @@ public class FamilyInSpaceTest {
 		ActionSpace actionSpace = gameLogic.getGame().getBoard().getTower("GreenTower").getFloors().get(1).getActionSpace(); //prendo il secondo piano della greenTower
 		
 		FamilyInSpaceAction familyInSpaceAction = new FamilyInSpaceAction(aManager, blackFamilyMember, actionSpace);
-		if (blackFamilyMember.getValue() >= 3) //necessario in quanto il valore e' random
-			Assert.assertTrue(familyInSpaceAction.isLegal()); //possibile, in quanto il familiare ha valore maggiore o uguale allo spazio azione
-		else 
+		if (blackFamilyMember.getValue() >= 3){ //necessario in quanto il valore e' random
+			Assert.assertTrue(familyInSpaceAction.checkActionCost());
+			Assert.assertTrue(familyInSpaceAction.notifyConfirm(new ConfirmViewEvent(true))); //possibile, in quanto il familiare ha valore maggiore o uguale allo spazio azione
+		}
+		else {
 			Assert.assertFalse(familyInSpaceAction.isLegal());
+			Assert.assertFalse(familyInSpaceAction.checkActionCost());
+		}
 		
+		familyInSpaceAction.perform();
+		Assert.assertFalse(familyInSpaceAction.notifyConfirm(new ConfirmViewEvent(true))); //e' stata fatta la perform. non puo' piu' essere fatta l'azione
+		familyInSpaceAction.setAlreadyDone(true);
+		familyInSpaceAction.perform(); // per l'altro branch nell'if
+		Assert.assertFalse(familyInSpaceAction.isLegal());
 		
+		actionSpace = new ActionSpace(8);
+		familyInSpaceAction = new FamilyInSpaceAction(aManager, blackFamilyMember, actionSpace);
+		Assert.assertFalse(familyInSpaceAction.checkActionCost());
 	}
 
 }
