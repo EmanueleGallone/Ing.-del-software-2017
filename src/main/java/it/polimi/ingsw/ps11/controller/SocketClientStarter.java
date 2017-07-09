017e067e0ea2659977bb3db0fc8e4c229c34d589
EmanueleGallone <emanuele.gallone@mail.polimi.it>
2017-07-09 21:28:39 +0200

    added tests

--- src/main/java/it/polimi/ingsw/ps11/controller/SocketClientStarter.java
+++ src/main/java/it/polimi/ingsw/ps11/controller/SocketClientStarter.java
@@ -3,7 +3,6 @@ package it.polimi.ingsw.ps11.controller;
 import it.polimi.ingsw.ps11.controller.client.Client;
 import it.polimi.ingsw.ps11.controller.network.socket.SocketConnection;
 import it.polimi.ingsw.ps11.view.graphicView.GraphicView;
-import it.polimi.ingsw.ps11.view.textualView.TextualView;
 
 public class SocketClientStarter {
 	public static void main(String[] args) {
