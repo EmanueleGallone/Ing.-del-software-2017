d688decb3e5467b9188bdf21af66b7209310d3b3
GabLogronio <gabriel.logronio@mail.polimi.it>
2017-07-09 16:55:06 +0200

    Cards names

--- src/test/java/it/polimi/ingsw/ps11/states/WaitResourceTest.java
+++ src/test/java/it/polimi/ingsw/ps11/states/WaitResourceTest.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.ps11.states;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import it.polimi.ingsw.ps11.controller.server.gameServer.PlayerFactory;
@@ -11,10 +13,6 @@ public class WaitResourceTest {
 	public void test(){
 		
 		PlayerFactory playerFactory = new PlayerFactory();
-		Player player1 = playerFactory.newPlayer(0);
-		Player player2 = playerFactory.newPlayer(1);
-		Player player3 = playerFactory.newPlayer(2);
-		Player player4 = playerFactory.newPlayer(3);
 		
 		//GameLogic gameLogic = new GameLogic(players);
 		
