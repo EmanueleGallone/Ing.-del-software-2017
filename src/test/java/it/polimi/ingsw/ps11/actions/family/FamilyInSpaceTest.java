5709626456e8e987c5f239f3489795829a20afe3
EmanueleGallone <emanuele.gallone@mail.polimi.it>
2017-07-10 00:33:15 +0200

    fixed FamilyInSpaceTest

--- src/test/java/it/polimi/ingsw/ps11/actions/family/FamilyInSpaceTest.java
+++ src/test/java/it/polimi/ingsw/ps11/actions/family/FamilyInSpaceTest.java
@@ -1,37 +1,18 @@
 package it.polimi.ingsw.ps11.actions.family;
 
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import it.polimi.ingsw.ps11.controller.server.gameServer.PlayerFactory;
-import it.polimi.ingsw.ps11.model.dices.Dice;
-import it.polimi.ingsw.ps11.model.dices.DiceProxy;
-import it.polimi.ingsw.ps11.model.familyMember.FamilyMember;
 import it.polimi.ingsw.ps11.model.familyMember.list.BlackFamilyMember;
-import it.polimi.ingsw.ps11.model.familyMember.list.NeutralFamilyMember;
-import it.polimi.ingsw.ps11.model.familyMember.list.OrangeFamilyMember;
 import it.polimi.ingsw.ps11.model.gameLogics.GameLogic;
-import it.polimi.ingsw.ps11.model.gameLogics.State;
 import it.polimi.ingsw.ps11.model.gameLogics.StateHandler;
 import it.polimi.ingsw.ps11.model.gameLogics.actions.ActionManager;
 import it.polimi.ingsw.ps11.model.gameLogics.actions.family.FamilyInSpaceAction;
-import it.polimi.ingsw.ps11.model.gameLogics.actions.family.FamilyInTowerAction;
-import it.polimi.ingsw.ps11.model.gameLogics.actions.resources.DecrementAction;
 import it.polimi.ingsw.ps11.model.player.Player;
-import it.polimi.ingsw.ps11.model.resources.ResourceList;
-import it.polimi.ingsw.ps11.model.resources.list.Coin;
-import it.polimi.ingsw.ps11.model.resources.list.Servant;
-import it.polimi.ingsw.ps11.model.zones.Floor;
 import it.polimi.ingsw.ps11.model.zones.actionSpace.ActionSpace;
-import it.polimi.ingsw.ps11.model.zones.towers.Tower;
-import it.polimi.ingsw.ps11.view.viewEvents.ConfirmViewEvent;
 
 public class FamilyInSpaceTest {
 	
@@ -58,7 +39,7 @@ public class FamilyInSpaceTest {
 		ActionSpace actionSpace = gameLogic.getGame().getBoard().getTower("GreenTower").getFloors().get(1).getActionSpace(); //prendo il secondo piano della greenTower
 		
 		FamilyInSpaceAction familyInSpaceAction = new FamilyInSpaceAction(aManager, blackFamilyMember, actionSpace);
-		Assert.assertFalse(familyInSpaceAction.isLegal()); //non e' possibile in quanto il valueCost dell'actionSpace e' maggiore del valore del familiare
+		Assert.assertTrue(familyInSpaceAction.isLegal()); //non e' possibile in quanto il valueCost dell'actionSpace e' maggiore del valore del familiare
 		
 	}
 
