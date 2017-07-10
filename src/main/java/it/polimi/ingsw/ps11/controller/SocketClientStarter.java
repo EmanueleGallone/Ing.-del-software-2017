a526b44212a637f9c338ddf0640bb0279e38a969
EmanueleGallone <emanuele.gallone@mail.polimi.it>
2017-07-10 21:28:02 +0200

    added uml.png files

--- src/main/java/it/polimi/ingsw/ps11/controller/SocketClientStarter.java
+++ src/main/java/it/polimi/ingsw/ps11/controller/SocketClientStarter.java
@@ -2,7 +2,6 @@ package it.polimi.ingsw.ps11.controller;
 
 import it.polimi.ingsw.ps11.controller.client.Client;
 import it.polimi.ingsw.ps11.controller.network.socket.SocketConnection;
-import it.polimi.ingsw.ps11.view.graphicView.GraphicView;
 import it.polimi.ingsw.ps11.view.textualView.TextualView;
 
 public class SocketClientStarter {
